10302e88cf5a7ae45cc4c2b3109965a7c3f5495d
sorecauadrian <a.sorecau@aico.swiss>
2024-07-05 17:37:25 +0300

    played with the input forms

--- app/Filament/Resources/ConferenceResource.php
+++ app/Filament/Resources/ConferenceResource.php
@@ -24,18 +24,24 @@ class ConferenceResource extends Resource
         return $form
             ->schema([
                 Forms\Components\TextInput::make('name')
+                    ->label('Conference Name')
                     ->required()
                     ->maxLength(255),
-                Forms\Components\TextInput::make('description')
-                    ->required()
-                    ->maxLength(255),
+                Forms\Components\MarkdownEditor::make('description')
+                    ->required(),
                 Forms\Components\DateTimePicker::make('start_date')
                     ->required(),
                 Forms\Components\DateTimePicker::make('end_date')
                     ->required(),
-                Forms\Components\TextInput::make('status')
-                    ->required()
-                    ->maxLength(255),
+                Forms\Components\Checkbox::make('is_published')
+                    ->default(true),
+                Forms\Components\Select::make('status')
+                    ->options([
+                        'draft' => 'Draft',
+                        'published' => 'Published',
+                        'cancelled' => 'Cancelled',
+                    ])
+                    ->required(),
                 Forms\Components\TextInput::make('region')
                     ->required()
                     ->maxLength(255),
