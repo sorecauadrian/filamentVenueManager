2ae4630e9171f067db67f1ef3f9e3ba617b8c33e
sorecauadrian <a.sorecau@aico.swiss>
2024-07-24 16:35:10 +0300

    file uploading functionality

--- app/Models/Speaker.php
+++ app/Models/Speaker.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Filament\Forms\Components\CheckboxList;
+use Filament\Forms\Components\FileUpload;
 use Filament\Forms\Components\Textarea;
 use Filament\Forms\Components\TextInput;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
@@ -29,15 +30,18 @@ class Speaker extends Model
             TextInput::make('name')
                 ->required()
                 ->maxLength(255),
+            FileUpload::make('avatar')
+                ->avatar()
+                ->directory('avatars')
+                ->imageEditor()
+                ->maxSize(1024 * 1024 * 10),
             TextInput::make('email')
                 ->email()
                 ->required()
                 ->maxLength(255),
             Textarea::make('bio')
-                ->required()
                 ->columnSpanFull(),
             TextInput::make('twitter_handle')
-                ->required()
                 ->maxLength(255),
             CheckboxList::make('qualifications')
                 ->columnSpanFull()
